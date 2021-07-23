1ac7beceb8eac9d329d85ac5565f24f8a531630b
Ussrfantom <dimkakrohin@mail.ru>
2021-07-23 04:00:52 +0300

    checked the adapter

---
+++ app/src/main/java/ussrfantom/com/example/bullinventorybyscanner/adapters/EmployeeAdapter.java
@@ -0,0 +1,158 @@
+package ussrfantom.com.example.bullinventorybyscanner.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import ussrfantom.com.example.bullinventorybyscanner.R;
+import ussrfantom.com.example.bullinventorybyscanner.pojo.Shop;
+
+public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeAdapter.EmployeeViewHolder> {
+
+
+    private List<Shop> shops;  //!!!!!!!!!!!
+
+    public List<Shop> getShops() {   //!!!!!!!!!!
+        return shops;
+    }
+
+    public void setShops(List<Shop> shops) { //!!!!!!!!!!
+        this.shops = shops;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public EmployeeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.shop_item, parent, false);
+        return new EmployeeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  EmployeeViewHolder employeeViewHolder, int position) {
+    Shop shop = shops.get(position);
+        employeeViewHolder.textView_name.setText(shop.getName());
+        employeeViewHolder.textView_name_pcd.setText(shop.getNamePcd());
+        employeeViewHolder.textView_pcd_serial_number.setText(shop.getPcdSerialNumber());
+        employeeViewHolder.textView_pcd_ke.setText(shop.getPcdKe());
+        employeeViewHolder.textView_name_server.setText(shop.getNameServer());
+        employeeViewHolder.textView_server_serial_number.setText(shop.getServerSerialNumber());
+        employeeViewHolder.textView_server_ke.setText(shop.getServerKe());
+        employeeViewHolder.textView_router_master.setText(shop.getRouterMaster());
+        employeeViewHolder.textView_router_master_serial_number.setText(shop.getRouterMasterSerialNumber());
+        employeeViewHolder.textView_router_master_ke.setText(shop.getRouterMasterKe());
+        employeeViewHolder.textView_router_reserve.setText(shop.getRouterReserve());
+        employeeViewHolder.textView_router_reserve_serial_number.setText(shop.getRouterReserveSerialNumber());
+        employeeViewHolder.textView_router_reserve_ke.setText(shop.getRouterReserveKe());
+        employeeViewHolder.textView_ups_pcd.setText(shop.getUpsPcd());
+        employeeViewHolder.textView_ups_pcd_serial_number.setText(shop.getUpsPcdSerialNumber());
+        employeeViewHolder.textView_ups_pcd_ke.setText(shop.getUpsPcdKe());
+        employeeViewHolder.textView_ups_server.setText(shop.getUpsServer());
+        employeeViewHolder.textView_ups_server_serial_number.setText(shop.getUpsServerSerialNumber());
+        employeeViewHolder.textView_ups_server_ke.setText(shop.getUpsServerKe());
+        employeeViewHolder.textView_monitor_pcd.setText(shop.getMonitorPcd());
+        employeeViewHolder.textView_monitor_pcd_serial_number.setText(shop.getMonitorPcdSerialNumber());
+        employeeViewHolder.textView_monitor_pcd_ke.setText(shop.getMonitorPcdKe());
+        employeeViewHolder.textView_print_pcd.setText(shop.getPrintPcd());
+        employeeViewHolder.textView_print_pcd_serial_number.setText(shop.getPrintPcdSerialNumber());
+        employeeViewHolder.textView_print_pcd_ke.setText(shop.getPrintPcdKe());
+        employeeViewHolder.textView_tsd1.setText(shop.getTsd1());
+        employeeViewHolder.textView_tsd1_serial_number.setText(shop.getTsd1SerialNumber());
+        employeeViewHolder.textView_tsd1_pcd_ke.setText(shop.getTsd1PcdKe());
+        employeeViewHolder.textView_tsd2.setText(shop.getTsd2());
+        employeeViewHolder.textView_tsd2_serial_number.setText(shop.getTsd2SerialNumber());
+        employeeViewHolder.textView_tsd2_pcd_ke.setText(shop.getTsd2PcdKe());
+        employeeViewHolder.textView_tsd3.setText(shop.getTsd3());
+        employeeViewHolder.textView_tsd3_serial_number.setText(shop.getTsd3SerialNumber());
+        employeeViewHolder.textView_tsd3_pcd_ke.setText(shop.getTsd3PcdKe());
+    }
+
+    @Override
+    public int getItemCount() {
+        return shops.size();
+    }
+
+    class EmployeeViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView_name;
+        TextView textView_name_pcd;
+        TextView textView_pcd_serial_number;
+        TextView textView_pcd_ke;
+        TextView textView_name_server;
+        TextView textView_server_serial_number;
+        TextView textView_server_ke;
+        TextView textView_router_master;
+        TextView textView_router_master_serial_number;
+        TextView textView_router_master_ke;
+        TextView textView_router_reserve;
+        TextView textView_router_reserve_serial_number;
+        TextView textView_router_reserve_ke;
+        TextView textView_ups_pcd;
+        TextView textView_ups_pcd_serial_number;
+        TextView textView_ups_pcd_ke;
+        TextView textView_ups_server;
+        TextView textView_ups_server_serial_number;
+        TextView textView_ups_server_ke;
+        TextView textView_monitor_pcd;
+        TextView textView_monitor_pcd_serial_number;
+        TextView textView_monitor_pcd_ke;
+        TextView textView_print_pcd;
+        TextView textView_print_pcd_serial_number;
+        TextView textView_print_pcd_ke;
+        TextView textView_tsd1;
+        TextView textView_tsd1_serial_number;
+        TextView textView_tsd1_pcd_ke;
+        TextView textView_tsd2;
+        TextView textView_tsd2_serial_number;
+        TextView textView_tsd2_pcd_ke;
+        TextView textView_tsd3;
+        TextView textView_tsd3_serial_number;
+        TextView textView_tsd3_pcd_ke;
+
+
+        public EmployeeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView_name = itemView.findViewById(R.id.textView_name);
+            textView_name_pcd = itemView.findViewById(R.id.textViewName_pcd);
+            textView_pcd_serial_number = itemView.findViewById(R.id.textView_pcd_serial_number);
+            textView_pcd_ke = itemView.findViewById(R.id.textView_pcd_ke);
+            textView_name_server = itemView.findViewById(R.id.textView_name_server);
+            textView_server_serial_number = itemView.findViewById(R.id.textView_server_serial_number);
+            textView_server_ke = itemView.findViewById(R.id.textView_server_ke);
+            textView_router_master = itemView.findViewById(R.id.textView_router_master);
+            textView_router_master_serial_number = itemView.findViewById(R.id.textView_router_master_serial_number);
+            textView_router_master_ke = itemView.findViewById(R.id.textView_router_master_ke);
+            textView_router_reserve = itemView.findViewById(R.id.textView_router_reserve);
+            textView_router_reserve_serial_number = itemView.findViewById(R.id.textView_router_reserve_serial_number);
+            textView_router_reserve_ke = itemView.findViewById(R.id.textView_router_reserve_ke);
+            textView_ups_pcd = itemView.findViewById(R.id.textView_ups_pcd);
+            textView_ups_pcd_serial_number = itemView.findViewById(R.id.textView_ups_pcd_serial_number);
+            textView_ups_pcd_ke = itemView.findViewById(R.id.textView_ups_pcd_ke);
+            textView_ups_server = itemView.findViewById(R.id.textView_ups_server);
+            textView_ups_server_serial_number = itemView.findViewById(R.id.textView_ups_server_serial_number);
+            textView_ups_server_ke = itemView.findViewById(R.id.textView_ups_server_ke);
+            textView_monitor_pcd = itemView.findViewById(R.id.textView_monitor_pcd);
+            textView_monitor_pcd_serial_number = itemView.findViewById(R.id.textView_monitor_pcd_serial_number);
+            textView_monitor_pcd_ke = itemView.findViewById(R.id.textView_monitor_pcd_ke);
+            textView_print_pcd = itemView.findViewById(R.id.textView_print_pcd);
+            textView_print_pcd_serial_number = itemView.findViewById(R.id.textView_print_pcd_serial_number);
+            textView_print_pcd_ke = itemView.findViewById(R.id.textView_print_pcd_ke);
+            textView_tsd1 = itemView.findViewById(R.id.textView_tsd1);
+            textView_tsd1_serial_number = itemView.findViewById(R.id.textView_tsd1_serial_number);
+            textView_tsd1_pcd_ke = itemView.findViewById(R.id.textView_tsd1_pcd_ke);
+            textView_tsd2 = itemView.findViewById(R.id.textView_tsd2);
+            textView_tsd2_serial_number = itemView.findViewById(R.id.textView_tsd2_serial_number);
+            textView_tsd2_pcd_ke = itemView.findViewById(R.id.textView_tsd2_pcd_ke);
+            textView_tsd3 = itemView.findViewById(R.id.textView_tsd3);
+            textView_tsd3_serial_number = itemView.findViewById(R.id.textView_tsd3_serial_number);
+            textView_tsd3_pcd_ke = itemView.findViewById(R.id.textView_tsd3_pcd_ke);
+
+        }
+    }
+}
--- app/src/main/java/ussrfantom/com/example/bullinventorybyscanner/searchke/SearchKe.java
+++ app/src/main/java/ussrfantom/com/example/bullinventorybyscanner/searchke/SearchKe.java
@@ -1,6 +1,8 @@
 package ussrfantom.com.example.bullinventorybyscanner.searchke;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,10 +13,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ussrfantom.com.example.bullinventorybyscanner.R;
+import ussrfantom.com.example.bullinventorybyscanner.adapters.EmployeeAdapter;
+import ussrfantom.com.example.bullinventorybyscanner.pojo.Shop;
 
 public class SearchKe extends AppCompatActivity {
 
+    private RecyclerView recyclerViewShop;
+    private EmployeeAdapter adapter;
     private Button buttonSearch;
     private Spinner spinnerSearch;
     private String stringShop;
@@ -25,9 +34,60 @@ public class SearchKe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serach_ke);
-
         spinnerSearch = findViewById(R.id.spinnerShops);
         buttonSearch = findViewById(R.id.buttonSearch);
+        recyclerViewShop = findViewById(R.id.recyclerViewShop);
+        adapter = new EmployeeAdapter();
+        recyclerViewShop.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewShop.setAdapter(adapter);
+        List<Shop> shops = new ArrayList<>();
+        Shop shop1 = new Shop();
+        shop1.setName("Каджары");
+        shop1.setNamePcd("Кмопьютер директора");
+        shop1.setPcdSerialNumber("2312312");
+        shop1.setPcdKe("2312312");
+        shop1.setNameServer("Сервер");
+        shop1.setServerSerialNumber("2312312");
+        shop1.setServerKe("2312312");
+        shop1.setRouterMaster("Роутер Мастер");
+        shop1.setRouterMasterSerialNumber("2312312");
+        shop1.setRouterMasterKe("2312312");
+        shop1.setRouterReserve("Роутер резерв");
+        shop1.setRouterReserveSerialNumber("2312312");
+        shop1.setRouterReserveKe("2312312");
+        shop1.setUpsPcd("ИБП ПКД");
+        shop1.setUpsPcdSerialNumber("2312312");
+        shop1.setUpsPcdKe("2312312");
+        shop1.setUpsServer("ИБП сервер");
+        shop1.setUpsServerSerialNumber("2312312");
+        shop1.setUpsServerKe("2312312");
+        shop1.setMonitorPcd("Монитор");
+        shop1.setMonitorPcdSerialNumber("2312312");
+        shop1.setMonitorPcdKe("2312312");
+        shop1.setPrintPcd("Принтер");
+        shop1.setPrintPcdSerialNumber("2312312");
+        shop1.setPrintPcdKe("2312312");
+        shop1.setTsd1("ТСД1");
+        shop1.setTsd1SerialNumber("2312312");
+        shop1.setTsd1PcdKe("2312312");
+        shop1.setTsd2("ТСД2");
+        shop1.setTsd2SerialNumber("2312312");
+        shop1.setTsd2PcdKe("2312312");
+        shop1.setTsd3("ТСД3");
+        shop1.setTsd3SerialNumber("1241241421");
+        shop1.setTsd3PcdKe("3123123121");
+        shops.add(shop1);
+        adapter.setShops(shops);
+
+
+
+
+
+
+
+
+
+
 
         //получаем магазин при нажатии кнопки начало
         buttonSearch.setOnClickListener(new View.OnClickListener() {
